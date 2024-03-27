ca308456793a199d5c099b09f7e7005718622438
Avcu18 <bora.avcu@rwth-aachen.de>
2024-03-27 17:58:57 +0100

    Update flipAnalysis.py

--- flipAnalysis.py
+++ flipAnalysis.py
@@ -6,29 +6,32 @@ url = 'https://flip.gg/wheel'
 
 # Webseite abrufen
 response = requests.get(url)
-
+print(response.text)
 #  check status 200
 if response.status_code == 200:
-
+    print('successful response!')
     soup = BeautifulSoup(response.text, 'html.parser')
-
- 
-    divs = soup.find_all('div', class_='tss-visq3v-right MuiBox-root mui-0')
-
-    farben = []
+   
+    oberes_div = soup.find('div', class_='tss-visq3v-right MuiBox-root mui-0')
+    print(oberes_div)
+    if oberes_div:
+        print('oberes <div>-Element gefunden.')
+        innere_divs = oberes_div.find_all('div', class_='tss-1kk5vra-root MuiBox-root mui-y6ur6a')
+        farben = []
 
    
-    for div in divs:
+        for div in divs:
   
-        inner_divs = div.find_all('div', attrs={'bg': True})
+            style = div.get('style')
         
         
-        for inner_div in inner_divs:
-            farbe = inner_div.get('bg')  # extract bg
-            if farbe:  
-                farben.append(farbe)
+            if style and 'background' in style:
+                value = style.split('background:')[-1].split(';')[0].strip()
+                farben.append(value)
 
-  
-    print(farben)
+        
+            print(farben)
+        else:
+            print('Übergeordnetes <div>-Element wurde nicht gefunden.')
 else:
     print(f'Fehler beim Abrufen der Webseite: Status Code {response.status_code}')
